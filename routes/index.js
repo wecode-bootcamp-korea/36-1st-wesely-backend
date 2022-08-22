a3381f208b1a8274449ee146eabada1120378bfc
JeongJaeHa <jaeha2324@gmail.com>
2022-08-22 21:14:30 +0900

    라우터 분기 수정

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,6 @@ const router = express.Router();
 const cartRouter = require("./cartRouter");
 
 router.use("/cart", cartRouter.router);
-router.use("/viewPosts", cartRouter.router);
 
 module.exports = router;
 
